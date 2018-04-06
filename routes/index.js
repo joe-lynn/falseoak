bee36f8fd04173010c216d6b08dd87fe6b65170b
Noah Frank <naf9685@rit.edu>
2018-04-06 00:28:39 -0400

    -Added /register page -Make simple landing page -Make layout more dynamic -Created register page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get('/register', function(req, res, next) {
+    res.render('register', { whatToSend: 'Nerfin' });
+});
+
+router.post('/register', function(req, res, next) {
+    res.redirect('/login');
 });
 
 module.exports = router;
